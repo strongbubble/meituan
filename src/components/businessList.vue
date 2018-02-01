36bdaf15ce3154064a5217867b1a526f54f13226
strongbubble <1303929162@qq.com>
2018-02-01 14:15:31 +0800

    图片加菜单切换

--- src/components/businessList.vue
+++ src/components/businessList.vue
@@ -6,11 +6,24 @@
 		</header>
 		<nav>
 			<ul>
-				<li>点菜</li>
-				<li>评价</li>
-				<li>商家</li>
+				<router-link to="/businessList/orderDishes/dishes">
+					<li @click="switchFn2('orderDishes')" :class="selectedO">
+						<a href="javascript:void(0);">点菜</a>
+					</li>
+				</router-link>
+				<router-link to="/businessList/orderDishes/evaluate">
+					<li @click="switchFn2('evaluate')" :class="selectedE">
+						<a href="javascript:void(0);">评价</a>
+					</li>
+				</router-link>
+				<router-link to="businessList/orderDishes/seller">
+					<li @click="switchFn2('seller')" :class="selectedS" >
+						<a href="javascript:void(0);">商家</a>
+					</li>
+				</router-link>
 			</ul>
 		</nav>
+		<router-view></router-view>
 	</div>
 </template>
 
@@ -138,27 +151,62 @@
 		overflow: hidden;
 		white-space: nowrap;
 		text-overflow: ellipsis;
-		margin: 0 65px;
-		text-align: center;
-		line-height: 44px;
-		font-size: 17px;
+		margin: 0 1.710526rem;
+		line-height: 1.157894rem;
+		font-size: 0.447368rem;
 		color: #444;
 		background: #fff;
+	}
+	
+	header {
+		display: flex;
+		justify-content: space-between;
+		background: #fff;
+		border-bottom: 1px solid #ddd;
+	}
+	
+	header span {
+		padding: 0 30px 0 8px;
+		display: inline-block;
+		vertical-align: top;
+		margin-top: 11px;
+	}
+	
+	header span img {
+		height: 0.526315rem;
 	}
 	
 	nav,
 	nav ul {
 		width: 100%;
+		display: flex;
+		justify-content: space-around;
 		height: auto;
+		background: #fff;
 		overflow: hidden;
 	}
 	
 	nav ul li {
-		float: left;
 		height: 1.18421rem;
 		text-align: center;
-		background: #fff;
+	}
+	
+	nav ul li a {
+		display: inline-block;
+		height: 1.18421rem;
+		line-height: 1.18421rem;
+		padding: 0 0.210526rem;
 		font-size: 0.421052rem;
+		color: #666;
+	}
+	
+	nav ul li .selected a {
+		border-bottom: 4px solid #FFD161;
+		color: #333;
+	}
+	
+	nav ul li .rmSelected a {
+		border-bottom: none;
 		color: #666;
 	}
 </style>
